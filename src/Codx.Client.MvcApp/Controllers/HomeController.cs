62dee953fae7a38d5f105382af7f42066dd28343
Code Explore <codeandexplore@gmail.com>
2021-05-03 01:44:14 +0800

    Initial

---
+++ src/Codx.Client.MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Codx.Client.MvcApp.Models;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Client.MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        public async Task<JsonResult> GetUser()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var user = User;
+
+            var data = new {
+                access_token = accessToken,
+                userclaims = user.Claims.Select(o => new { Type=o.Type, Value = o.Value }).ToList()
+            };
+
+            return Json(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult LogOff()
+        {
+            return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
